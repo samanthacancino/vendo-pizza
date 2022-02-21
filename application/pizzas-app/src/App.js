7221097cc0f51894473c1b1a9e010162236b2fb7
Samantha Cancino <samanthacancino@gmail.com>
2022-02-21 17:56:47 +0100

    React added

---
+++ application/pizzas-app/src/App.js
@@ -0,0 +1,12 @@
+import PizzasList from './components/PizzasList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <PizzasList />
+    </div>
+  );
+}
+
+export default App;
